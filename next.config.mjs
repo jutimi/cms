ffbce5bd603777609077e3249ff315afdf79e08c
Duong Son <duongson212998@gmail.com>
2024-08-19 18:16:38 +0700

    feat: i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from "next-intl/plugin";
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin("./src/i18n/i18n.ts");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default withNextIntl(nextConfig);
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,95 +1,51 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import { Menu, MenuProps } from "antd";
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
+import Sider from "antd/es/layout/Sider";
+import { useRouter } from "next/navigation";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
-const items: MenuItem[] = [
-  {
-    key: "sub1",
-    label: "Navigation One",
-    icon: <MailOutlined />,
-    children: [
-      {
-        key: "g1",
-        label: "Item 1",
-        type: "group",
-        children: [
-          { key: "1", label: "Option 1" },
-          { key: "2", label: "Option 2" },
-        ],
-      },
-      {
-        key: "g2",
-        label: "Item 2",
-        type: "group",
-        children: [
-          { key: "3", label: "Option 3" },
-          { key: "4", label: "Option 4" },
-        ],
-      },
-    ],
-  },
-  {
-    key: "sub2",
-    label: "Navigation Two",
-    icon: <AppstoreOutlined />,
-    children: [
-      { key: "5", label: "Option 5" },
-      { key: "6", label: "Option 6" },
-      {
-        key: "sub3",
-        label: "Submenu",
-        children: [
-          { key: "7", label: "Option 7" },
-          { key: "8", label: "Option 8" },
-        ],
-      },
-    ],
-  },
-  {
-    type: "divider",
-  },
-  {
-    key: "sub4",
-    label: "Navigation Three",
-    icon: <SettingOutlined />,
-    children: [
-      { key: "9", label: "Option 9" },
-      { key: "10", label: "Option 10" },
-      { key: "11", label: "Option 11" },
-      { key: "12", label: "Option 12" },
-    ],
-  },
-  {
-    key: "grp",
-    label: "Group",
-    type: "group",
-    children: [
-      { key: "13", label: "Option 13" },
-      { key: "14", label: "Option 14" },
-    ],
-  },
-];
+function getItem(t: any): MenuItem[] {
+  return [
+    {
+      key: "workspace_group",
+      type: "group",
+      label: t("workspace_group"),
+      children: [
+        {
+          key: "user_workspaces",
+          label: t("user_workspaces"),
+        },
+        {
+          key: "organizations",
+          label: t("organizations"),
+        },
+      ],
+    },
+  ];
+}
 
 export function NavBar() {
+  const t = useTranslations("nav_bar");
+  const router = useRouter();
+
   const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
+    router.push(`/${e.key}`);
   };
 
   return (
-    <Menu
-      onClick={onClick}
-      defaultSelectedKeys={["1"]}
-      defaultOpenKeys={["sub1"]}
-      mode="inline"
-      items={items}
-      theme="dark"
-    />
+    <Sider collapsible>
+      <div className="demo-logo-vertical" />
+      <Menu
+        onClick={onClick}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        items={getItem(t)}
+        theme="dark"
+      />
+    </Sider>
   );
 }
