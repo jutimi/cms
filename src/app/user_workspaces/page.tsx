03b67672105e1165de0e6ed19ba3e3472c6c2cec
Duong Son <duongson212998@gmail.com>
2024-08-21 18:13:10 +0700

    feat: update user_workspaces list page

--- src/app/user_workspaces/page.tsx
+++ src/app/user_workspaces/page.tsx
@@ -1,3 +1,12 @@
+import { Col, Row } from "antd";
+
 export default function Page() {
-    return "User Workspaces";
+    return (
+        <>
+            <Row>
+                <Col span={8}>Organization List</Col>
+                <Col span={16}>User Workspace List</Col>
+            </Row>
+        </>
+    );
 }
